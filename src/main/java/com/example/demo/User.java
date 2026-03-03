b4720df6b97fdf5f6c5e6f3f9fe273ca067c9391
Ivánácz Péter <forint.peti@gmail.com>
2026-03-03 15:56:57 +0100

    added required email at registration for future implementations

--- src/main/java/com/example/demo/User.java
+++ src/main/java/com/example/demo/User.java
@@ -16,6 +16,9 @@ public class User {
     @Column(nullable = false)
     private String password;
 
+    @Column(nullable = false, unique = true)
+    private String email;
+
     public User() {}
 
     public User(String username, String password) {
@@ -31,4 +34,7 @@ public class User {
 
     public String getPassword() { return password; }
     public void setPassword(String password) { this.password = password; }
+
+    public String getEmail() {return email;}
+    public void setEmail(String email) { this.email = email;}
 }
